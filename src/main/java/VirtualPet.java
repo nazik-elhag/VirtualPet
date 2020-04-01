f2ad110810a9ed2f403ac1109f2d4c426ca9463a
Shahd Osman <Shahd_aa@live.com>
2020-04-01 19:13:19 -0400

    Add getter

---
+++ src/main/java/VirtualPet.java
@@ -0,0 +1,15 @@
+
+public class VirtualPet {
+ 
+	protected String petName;
+
+	public String getPetName() {
+		return petName;
+	}
+
+	public void setPetName(String petName) {
+		this.petName = petName;
+	} 
+	
+	
+}
