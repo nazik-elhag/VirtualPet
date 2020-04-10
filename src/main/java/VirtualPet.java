364f84085219c80c35e66506db67969926ded632
tagwaesam <tagwaesam5@gmail.com>
2020-04-10 02:53:53 -1100

    add test to virtualtest and add virtualpet method

--- src/main/java/VirtualPet.java
+++ src/main/java/VirtualPet.java
@@ -2,14 +2,24 @@
 public class VirtualPet {
  
 	protected String petName;
-
-	public String getPetName() {
+	protected int getHunger;
+	protected int energy;
+	VirtualPet(String petName)
+	{
+		this.petName = petName;
+		this.getHunger = 10;
+		this.energy=100;
+	}
+	public String getName() {
 		return petName;
 	}
 
-	public void setPetName(String petName) {
-		this.petName = petName;  
-	} 
-	
-	
+	public int getHunger(){
+		return this.getHunger;
+	}
+	public void tick() {
+		this.getHunger +=10;
+		this.energy--;
+	}
+
 }
--- src/test/java/VirtualPetTest.java
+++ src/test/java/VirtualPetTest.java
@@ -1,5 +1,42 @@
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 public class VirtualPetTest {
-   
-	
+
+    @Test
+    public void shouldBeAbleToCreatePet() {
+        VirtualPet pet = new VirtualPet("cat1");
+    }
+
+    @Test
+    public void petShouldHaveName() {
+        VirtualPet underTest = new VirtualPet("Jeff");
+
+        String expected = underTest.getName();
+
+        assertEquals(expected, "Jeff");
+    }
+
+    @Test
+    public void shouldHaveDefaultHunger() {
+        VirtualPet underTest = new VirtualPet("Steve");
+
+        int expected = underTest.getHunger;
+
+        assertEquals(expected, 10);
+    }
+
+    @Test
+    public void shouldTick() {
+        VirtualPet pet = new VirtualPet("Kendrick");
+
+        int initialHunger = pet.getHunger();
+        pet.tick();
+        int hungerAfterTick = pet.getHunger();
+
+        assertEquals(initialHunger + 10, hungerAfterTick);
+    }
 }
+
